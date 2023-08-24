2e23d02bcf623f1d572dda024e74d9ea6958fa65
nirajgirixd <nirajgiri9860@gmail.com>
2023-08-24 19:05:23 +0545

    Initial commit

---
+++ CommandDefinition.py
@@ -0,0 +1,132 @@
+def menuDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tMenu shows available commands.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> menu\n")
+
+def newsDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tNews brings you the latest news by searching web.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> news\n")
+
+def extractDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tExtract gets data from web and saves it which is then used by the program to display appropriate results.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> extract\n")
+
+def companyDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tCompany shows details about a particular company.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> company")
+    print("\t\tCompany Name>   UPPER\n")
+
+def comparedef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tCompare allows user to compare between any two companies of any sector.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> compare")
+    print("\t\tFirst Company Symbol>  UPPER")
+    print("\t\tSecond Company Symbol> RADHI\n")
+
+def sortDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tSort displays data of selected sector with respect to the selected factor in a particular order.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> sort")
+    print("\t\tFactor>         price")
+    print("\t\tSector>         hydropower\n")
+
+def rangeDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tRange selects company within the given range of selected factor and sector.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> range")
+    print("\t\tSector>         finance")
+    print("\t\tFactor>         eps")
+    print("\t\tEnter lower limit: 20")
+    print("\t\tEnter upper limit: 100\n")
+
+def sectorDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tSector filters out other sector and display company data of particular sector mentioned by user.\n")
+    print("\t\tSectors:")
+    print("\t\t1. Commercial Bank")
+    print("\t\t2. Development Bank")
+    print("\t\t3. Finance")
+    print("\t\t4. Hydro Power")
+    print("\t\t5. Life Insurance")
+    print("\t\t6. Non-Life Insurance")
+    print("\t\t7. Microfinance")
+    print("\t\t8. Mutual Fund")
+    print("\t\t9. Investment")
+    print("\t\t10. Manufacturing And Processing")
+    print("\t\t11. Hotels And Tourism")
+    print("\t\t12. Reinsurance")
+    print("\t\t13. Tradings")
+    print("\t\t14. Others")
+    print("\t\t15. All")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> sector")
+    print("\t\tSector>         commercial bank\n")
+
+def chartDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tChart displays a simple chart of the mentioned company")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> chart")
+    print("\t\tCompany> upper\n")
+
+def reportDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tReport generates share report and predicts future outcome.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> report\n")
+
+def technicalDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tTechnical generates technical report of mentioned stock.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> technical")
+    print("\t\tCompany> upper\n")
+
+def dividendDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tDividend shows stocks that have declared dividend.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> dividend\n")
+
+def summaryDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tSummary gives an overview of the market.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> summary\n")
+
+def resetDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tReset creates new files by deleting all the existing files.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> reset\n")
+
+def exitDef():
+    print("\n\t-------------------------------------------------------------------------------------------------------------------\n")
+    print("\t\tDescription:")
+    print("\t\tExit terminates the program.")
+    print("\n\t\tFormat:")
+    print("\t\tStock Analyzer> exit\n")
